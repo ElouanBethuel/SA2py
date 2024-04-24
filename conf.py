834d5f01e23e79e66761fe748be435242a6a09d2
ELouan Bethuel <113098629+ElouanBethuel@users.noreply.github.com>
2024-04-24 15:57:25 +0200

    Create conf.py

---
+++ conf.py
@@ -0,0 +1,13 @@
+# Importez les modules nécessaires
+# import os
+# import sys
+
+# -- Project information -----------------------------------------------------
+
+project = 'SAS'
+copyright = '2024, AUTHOR'
+author = 'Elouan BETHUEL'
+
+# The short X.Y version
+version = '0.1'
+release = '0.1'
